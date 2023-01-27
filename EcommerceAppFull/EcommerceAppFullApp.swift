106448c7d13f030f35a3cf4dab89f1bf47c36e16
Jakhangir <Jakhangir799@gmail.com>
2023-01-27 23:53:11 +0000

    Initial Commit

---
+++ EcommerceAppFull/EcommerceAppFullApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppFullApp.swift
+//  EcommerceAppFull
+//
+//  Created by Jahongir on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppFullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
